1d9500e015d63f11347bc830371bef4e291e096f
Shirish Kawatkar <shirish@synerzip.com>
2015-09-11 16:10:40 +0530

    Added Capture Screenshot feature.

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -2,15 +2,20 @@ package base;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 
 
@@ -25,7 +30,7 @@ public abstract class TestBase {
 	/**
 	 * Initialize Logger.
 	 */
-	public static void initLogs(String Logclass){
+	protected static void initLogs(String Logclass){
 			
 			// Initialize Log4j logs 
 			DOMConfigurator.configure(System.getProperty("user.dir")+File.separator+"config"+File.separator+"log4j.xml");
@@ -62,7 +67,7 @@ public abstract class TestBase {
 	/**
 	 * Read Properties.
 	 */
-	public static String getPropertyValue(String PropertyKey){
+	protected static String getPropertyValue(String PropertyKey){
 		Properties props=null;
 		FileInputStream fin =null;
 		String PropertyValue = null;
@@ -81,6 +86,59 @@ public abstract class TestBase {
 		}
 	
 
+	/**
+	 * Define path for Screenshot file.
+	 */
+	protected String getScreenshotSavePath() {
+		String packageName = this.getClass().getPackage().getName();
+		File dir = new File(System.getProperty("user.dir")+File.separator+"screenshot"+File.separator + packageName + File.separator);
+		dir.mkdirs();
+		return dir.getAbsolutePath();
+	}
+	
+	
+	/**
+	 * Take Screenshot on failure.
+	 */
+	protected void getScreenshot() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+		String date = sdf.format(new Date());
+		String url = driver.getCurrentUrl().replaceAll("[\\/:*\\?\"<>\\|]", "_");
+		String ext = ".png";
+		String path = getScreenshotSavePath() + File.separator + date + "_" + url + ext;
+
+		try {
+			if (driver instanceof TakesScreenshot) {
+				File tmpFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				org.openqa.selenium.io.FileHandler.copy(tmpFile, new File(path));
+				log.error("Captured Screenshot for Failure: "+path);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	/**
+	 * Assert Actual and Expected Strings
+	 */
+	
+	protected void assertStrings(String actual, String expected){
+		
+		
+		try{
+			Assert.assertEquals(actual, expected);
+			log.info("Actual string: [ "+actual+" ] does match with Expected string: [ "+expected+" ]");		
+			
+		}catch(AssertionError e){
+			log.error("Actual string: [ "+actual+" ] does not match with Expected string: [ "+expected+" ]");
+			getScreenshot();
+			Assert.fail();
+			
+		}
+		
+	} 
+	
 	/**
 	 * Quit Driver.
 	 */
--- src/test/java/sanitySuite/TestCase_1_GoogleSearch.java
+++ src/test/java/sanitySuite/TestCase_1_GoogleSearch.java
@@ -3,10 +3,8 @@ package sanitySuite;
 import org.testng.annotations.Test;
 import base.TestBase;
 import pages.Syn_google_search;
-
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 
@@ -20,13 +18,18 @@ public class TestCase_1_GoogleSearch extends TestBase{
   @Test (priority=1, description = "Open Google Search URL")	
   public void open_url() {
 	  
-	  log.info("Open Google Search URL");
+	  log.info("Open Google Search URL.");
 	  driver.get(getPropertyValue("base.url"));
 	  
+	  log.info("Get input string from properties file and put it into the search box.");
 	  obj_google_search = new Syn_google_search (driver);
 	  obj_google_search.search_by_first_option(getPropertyValue("TestCase_1.searchString_1"));
-	  Assert.assertTrue(obj_google_search.get_first_option().equals(getPropertyValue("TestCase_1.assertString_1")), "Expected String doesn't match");
-	  
+
+	  log.info("Assert actual searched string with expected string from properties file.");
+	  assertStrings(obj_google_search.get_first_option(),getPropertyValue("TestCase_1.assertString_1"));
+
+//	  Assert.assertTrue(obj_google_search.get_first_option().equals(getPropertyValue("TestCase_1.assertString_1")));
+	
   }
   
   @Test (priority=2, description = "Click on first search option")	
